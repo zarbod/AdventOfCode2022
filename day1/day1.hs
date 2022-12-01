4754cc61f5799ef48da39d6204198f0a7d7046d4
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2022-12-01 12:04:33 -0600

    Add files via upload

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,12 +1,6 @@
 import System.IO
+import Data.List
 import Data.List.Split
-
-sort :: [Int] -> [Int]
-sort [] = []
-sort (x:xs) =
-  let smaller = sort [a | a <- xs, a < x]
-      larger = sort [a | a <- xs, a >= x]
-      in larger ++ [x] ++ smaller
 
 main :: IO ()
 main = do
@@ -17,5 +11,5 @@ main = do
   let calories' = map (\xs -> filter (/="") xs) calories
   let intCalories = map (map (\x -> read x::Int)) calories'
   let sums = foldr (\x acc -> (sum x):acc) [] intCalories
-  putStrLn $ show . sum . take 3 . sort $ sums
+  putStrLn $ show . sum . take 3 . reverse . sort $ sums
   hClose input
