98d0f0a2b5dc381db53aa8d7666a4a4ec3807578
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2022-12-02 01:11:31 -0600

    part 2

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,18 +1,20 @@
 import System.IO
 import Data.List.Split
 
-points :: [String] -> Int
-points xs
-  | b == "X"  = 1 + if a == "A" then 3 else if a == "B" then 0 else 6
-  | b == "Y"  = 2 + if a == "A" then 6 else if a == "B" then 3 else 0
-  | otherwise = 3 + if a == "A" then 0 else if a == "B" then 6 else 3
+points :: (Int, Int, Int, Int, Int, Int) -> Int -> [String] -> Int
+points (x1, x2, x3, x4, x5, x6) t xs
+  | b == "X"  = x1 + if a == "A" then x4 else if a == "B" then x5 else x6
+  | b == c    = x2 + if a == "A" then x6 else if a == "B" then x4 else x5
+  | otherwise = x3 + if a == "A" then x5 else if a == "B" then x6 else x4
   where a = head xs
         b = last xs
+        c = if t == 0 then "Y" else "Z"
 
 main :: IO ()
 main = do
   input <- openFile "input.txt" ReadMode
   contents <- hGetContents input
   let rps = filter (\x -> last x /= "") . map (splitOn " ") . splitOn "\n" $ contents
-  putStrLn $ show (sum . map (points) $ rps)
+  putStrLn $ show (sum . map (points (1, 2, 3, 3, 0, 6) 0) $ rps) -- part 1
+  putStrLn $ show (sum . map (points (0, 6, 3, 3, 1, 2) 1) $ rps) -- part 2
   hClose input
